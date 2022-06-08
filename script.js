4956e5727b6358dd3de0d692c5209dd4009e6ca0
Daniel <doluremi19@gmail.com>
2022-06-08 14:18:19 +0100

    updated styles and script

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let menu = document.querySelector("i[title='Open Menu']");
 let body = document.querySelector("body")
 let modal = document.querySelector("#modal")
+let closemenu = document.createElement('i')
+closemenu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>"
 
 console.log(menu);
 
-menu.addEventListener('onClick', function(e){
-    menu.innerHTML = "<i class="fa-solid fa-xmark"></i>"
+menu.addEventListener('click', function(e){
+    menu.parentElement.replaceChild(closemenu, menu)
+    //menu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>"
     body.style.overflow = "hidden";
     modal.style.display = "block"
 });
