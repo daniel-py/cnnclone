08da25156e115221ade4f273e5173cca85978eea
Daniel <doluremi19@gmail.com>
2022-06-08 17:32:49 +0100

    updated styles

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 let menu = document.querySelector("i[title='Open Menu']");
 let body = document.querySelector("body");
 let modal = document.querySelector("#modal");
-//let closemenu = document.createElement('i');
-//let menuhtml = "fa-solid fa-bars"
-//closemenu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>";
 check = 0;
 
 console.log(menu);
 
 menu.addEventListener('click', function(e){
     if (check === 0){
-        console.log(check)
         menu.className = "fa-solid fa-xmark"
         body.style.overflow = "hidden";
         modal.style.display = "block";
         check = 1;
-    }
-    else {
-        console.log(check)
-        menu.className = "fa-solid fa-bars"
+    } else {
+        menu.className = "fa-solid fa-bars";
         body.style.overflow = "auto";
         modal.style.display = "none";
-        check = 0
+        check = 0;
     }
 });
