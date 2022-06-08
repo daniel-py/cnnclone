bacb701941ecc83d62892fca24a06a664f0412ac
Daniel <doluremi19@gmail.com>
2022-06-08 17:20:39 +0100

    updated script

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let menu = document.querySelector("i[title='Open Menu']");
-let body = document.querySelector("body")
-let modal = document.querySelector("#modal")
-let closemenu = document.createElement('i')
-closemenu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>"
+let body = document.querySelector("body");
+let modal = document.querySelector("#modal");
+//let closemenu = document.createElement('i');
+//let menuhtml = "fa-solid fa-bars"
+//closemenu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>";
+check = 0;
 
 console.log(menu);
 
 menu.addEventListener('click', function(e){
-    menu.parentElement.replaceChild(closemenu, menu)
-    //menu.innerHTML = "<i class='fa-solid fa-xmark' title='Open Menu'></i>"
-    body.style.overflow = "hidden";
-    modal.style.display = "block"
+    if (check === 0){
+        console.log(check)
+        menu.className = "fa-solid fa-xmark"
+        body.style.overflow = "hidden";
+        modal.style.display = "block";
+        check = 1;
+    }
+    else {
+        console.log(check)
+        menu.className = "fa-solid fa-bars"
+        body.style.overflow = "auto";
+        modal.style.display = "none";
+        check = 0
+    }
 });
